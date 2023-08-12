9217b7c39e547d1d577fbbb87ca55a6386b771ff
Mukhammad019 <2016kosumov@mail.ru>
2023-08-12 14:57:17 +0300

    addContact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,15 +1,43 @@
 import React from "react";
 import s from "./Contact.module.scss";
-import { BsInstagram } from 'react-icons/bs'
-import { BsTelegram } from 'react-icons/bs'
-import { SlSocialVkontakte } from 'react-icons/sl'
+import { BsInstagram } from "react-icons/bs";
+import { BsTelegram } from "react-icons/bs";
+import { SlSocialVkontakte } from "react-icons/sl";
+import { GoMail } from "react-icons/go";
+import { BsTelephone } from "react-icons/bs";
 
 const About = () => {
   return (
     <div className={s.content}>
-      <a target="_blank" className="animate__animated animate__slideInLeft" href="https://instagram.com/kkkkkkkkkkkkkkkkkkk019?igshid=MzRlODBiNWFlZA=="><BsInstagram/><span>Instagram</span></a>
-      <div className="animate__animated animate__slideInDown"><a target="_blank" className={s.tg} href="https://t.me/kkk019"><BsTelegram/><span>Telegram</span></a></div>
-      <a target="_blank" className="animate__animated animate__slideInRight"  href="https://vk.com/kkk019"><SlSocialVkontakte/><span>VK</span></a>
+      <div className={s.left}>
+        <div className={s.mail}>
+          <GoMail />
+          2016kosumov@mail.ru
+        </div>
+        <div className={s.phone}>
+          <BsTelephone />
+          +7 989 904 93 24
+        </div>
+        <div className={s.inst}>
+          <BsInstagram />
+          kkkkkkkkkkkkkkkkkkk019
+        </div>
+      </div>
+      <div className={s.right}>
+        <div className="animate__animated animate__slideInDown">
+          <a target="_blank" className={s.tg} href="https://t.me/kkk019">
+            <BsTelegram />
+          </a>
+        </div>
+        <div className="animate__animated animate__slideInUp"><div className={s.vk}><a
+          target="_blank"
+          href="https://vk.com/kkk019"
+        >
+          VK
+        </a>
+        </div>
+        </div>
+      </div>
     </div>
   );
 };
