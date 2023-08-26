112ce38381e8a3bfd6679081cbf14fcc20c65619
Mukhammad019 <2016kosumov@mail.ru>
2023-08-26 23:49:12 +0300

    done

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,16 +3,11 @@ import s from "./Contact.module.scss";
 import { BsInstagram } from "react-icons/bs";
 import { GoMail } from "react-icons/go";
 import { BsTelephone } from "react-icons/bs";
-import { FaWhatsapp } from 'react-icons/fa'
-
+import { FaWhatsapp } from "react-icons/fa";
 
 const About = () => {
-
- 
-
   return (
     <div className={s.content}>
-      
       <div className={s.left}>
         <div className={s.mail}>
           <GoMail />
@@ -29,20 +24,17 @@ const About = () => {
       </div>
       <div className={s.right}>
         <div className="animate__animated animate__slideInDown">
-          <a target="_blank"  href="https://t.me/kkk019">
+          <a target="_blank" href="https://t.me/kkk019">
             <img className={s.tg} src="./src/img/tg.png" alt="" />
           </a>
         </div>
-        <div className="animate__animated animate__slideInUp"><div className={s.vk}><a
-          target="_blank"
-          href="https://vk.com/kkk019"
-        >
-          <img className={s.iconvk} src="./src/img/vk1.png" alt="" />
-        </a>
+        <div className="animate__animated animate__slideInUp">
+          <div className={s.vk}>
+            <a target="_blank" href="https://vk.com/kkk019">
+              <img className={s.iconvk} src="./src/img/vk1.png" alt="" />
+            </a>
+          </div>
         </div>
-        </div>
-
-        
       </div>
     </div>
   );
